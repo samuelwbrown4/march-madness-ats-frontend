78c2739ded001ba42058647e30e310031dc501d7
Samuel Brown <samuelwbrown4@gmail.com>
2026-03-14 10:37:40 -0400

    add landing page header

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,6 +8,7 @@ function LandingPage() {
 
     const [leagueId, setLeagueId] = useState('');
     const [showModal, setShowModal] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
     const navigate = useNavigate()
 
@@ -35,7 +36,7 @@ function LandingPage() {
     }
 
     return (
-        <div style={{backgroundColor: '#181a1b'}}>
+        <div style={{ backgroundColor: '#181a1b' }}>
             <Modal show={showModal} onHide={() => setShowModal(false)} size="lg">
                 <Modal.Header closeButton className="custom-modal-body">
                     <Modal.Title>About</Modal.Title>
@@ -95,14 +96,48 @@ function LandingPage() {
                 </Modal.Footer>
             </Modal>
             <div id="landing-page">
+                <div
+                    id="dark-header"
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        paddingTop: '20px',
+                        gap: '1rem'
+                    }}
+                >
+                    <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+
+                        <button className="hamburger-btn" onClick={() => setShowMenu(!showMenu)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                                <rect y="4" width="16" height="2" />
+                                <rect y="8" width="16" height="2" />
+                                <rect y="12" width="16" height="2" />
+                            </svg>
+                        </button>
+                        <div className={`header-buttons-container ${showMenu ? 'show' : ''}`}>
+                            <Link className="header-button" onClick={() => setShowModal(true)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="header-svg" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                                </svg>
+                                {`${showMenu ? 'About' : ''}`}
+                            </Link>
+                            <Link className="header-button" to='/admin-login'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" className="header-svg" viewBox="0 0 16 16">
+                                    <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                    <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492z" />
+                                </svg>
+                                {`${showMenu ? 'Admin' : ''}`}
+                            </Link>
+
+                        </div>
+                    </div>
+
+
+                </div>
                 <div id="welcome-header">
                     <h1>Welcome!</h1>
-                    <Link className="header-button" onClick={() => setShowModal(true)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="header-svg" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                            <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
-                        </svg>
-                    </Link>
                 </div>
                 <div className="league-sign-in">
                     <h3>League Sign In</h3>
@@ -116,7 +151,7 @@ function LandingPage() {
                         <span>Don't have a league? Create one <Link to='/create-league'>here</Link>.</span>
                     </div>
                 </div>
-                <div style={{textAlign: 'center'}}>
+                <div style={{ textAlign: 'center' }}>
                     <img src='/images/backdoor-brackets-logo.png' style={{ maxHeight: '200px', maxWidth: '75%' }} />
                 </div>
             </div>
